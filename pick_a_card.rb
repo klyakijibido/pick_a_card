67b1c77c84ab4b2b30a53b04cf1509fc0aa3cbd0
Yaroslav Kuzhakhmetov <93444486+klyakijibido@users.noreply.github.com>
2023-02-20 19:35:51 +0300

    Desk & Card classes

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -1,26 +1,12 @@
 puts 'Pick a card'
 
-values = ['2','3','4','5','6','7','8','9','10','J','Q','K','A']
-suits = ['Diamonds', 'Hearts', 'Clubs', 'Spades'] # Буби, Черви, Крести, Пики
+require_relative 'lib/desk'
 
-# puts "#{values.sample} of #{suits.sample}"
-cards = []
-
-values.each do |value|
-  suits.each do |suit|
-    cards << "#{value}#{suit[0].downcase}"
-  end
-end
-
-2.times do
-  cards << '[Joker]'
-end
-
-cards.shuffle!
+desk = Desk.new.shuffle
 
 puts 'How many cards do you pick?'
 number = $stdin.gets.to_i
 
 number.times do
-  puts cards.pop
+  puts desk.pop_random_card
 end
